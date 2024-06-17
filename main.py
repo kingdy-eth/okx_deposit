3e079e02e258bed7cd1ccdb90fc44cfc15a9eebe
kingdy <111358576+kingdy23@users.noreply.github.com>
2024-06-18 00:28:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,43 +22,44 @@ def main():
     for private_key, deposit_address in zip(private_keys, deposit_addresses):
         address = Web3.to_checksum_address(w3.eth.account.from_key(private_key).address)
 
-        while True:
-            balance_wei = w3.eth.get_balance(address)
-            amount = round(random.uniform(eth_threshold[0], eth_threshold[1]), decimal_places)
-            threshold_wei = int(amount * 1e18)
-            value_to_send = balance_wei - threshold_wei
+        balance_wei = w3.eth.get_balance(address)
+        amount = round(random.uniform(eth_threshold[0], eth_threshold[1]), decimal_places)
+        threshold_wei = int(amount * 1e18)
+        value_to_send = balance_wei - threshold_wei
 
-            if value_to_send <= 0:
-                logger.warning(f'{address} | Insufficient balance to send transaction')
-                break
+        if value_to_send <= 0:
+            logger.warning(f'{address} | Insufficient balance to send transaction')
+            continue
 
-            logger.info(f'{address} | Sending {value_to_send / 1e18} ETH to {deposit_address}')
+        logger.info(f'{address} | Sending {value_to_send / 1e18} ETH to {deposit_address}')
 
-            tx = {
-                'chainId': 324,
-                'nonce': w3.eth.get_transaction_count(address),
-                'from': address,
-                'to': Web3.to_checksum_address(deposit_address),
-                'value': value_to_send,
-                'gasPrice': w3.eth.gas_price
-            }
+        tx = {
+            'chainId': 324,
+            'nonce': w3.eth.get_transaction_count(address),
+            'from': address,
+            'to': Web3.to_checksum_address(deposit_address),
+            'value': value_to_send,
+            'gasPrice': w3.eth.gas_price
+        }
 
-            try:
-                tx['gas'] = int(w3.eth.estimate_gas(tx) * increase_gas)
-                sign = w3.eth.account.sign_transaction(tx, private_key)
-                tx_hash = w3.eth.send_raw_transaction(sign.rawTransaction)
+        try:
+            tx['gas'] = int(w3.eth.estimate_gas(tx) * increase_gas)
+            sign = w3.eth.account.sign_transaction(tx, private_key)
+            tx_hash = w3.eth.send_raw_transaction(sign.rawTransaction)
 
-                if verif_tx(w3, address, tx_hash):
-                    logger.success(f'{address} | Transaction was successful | Hash: {tx_hash.hex()}')
-                    logger.info(f'Transaction URL: https://explorer.zksync.io/tx/{tx_hash.hex()}')
-                else:
-                    logger.warning(f'{address} | Transaction failed | {tx_hash.hex()}')
-            except Exception as error:
-                logger.warning(f'{address} | Transaction failed | Error: {error}')
+            if verif_tx(w3, address, tx_hash):
+                logger.success(f'{address} | Transaction was successful | Hash: {tx_hash.hex()}')
+                logger.info(f'Transaction URL: https://explorer.zksync.io/tx/{tx_hash.hex()}')
+            else:
+                logger.warning(f'{address} | Transaction failed | {tx_hash.hex()}')
+        except Exception as error:
+            logger.warning(f'{address} | Transaction failed | Error: {error}')
 
         delay = random.randint(delay_between_accounts[0], delay_between_accounts[1])
         logger.info(f'Sleeping for {delay} seconds')
         time.sleep(delay)
+
+    logger.info("Finished processing all accounts")
 
 
 def verif_tx(w3, address, tx_hash):
